cdc399c0f2994c6704f77f6080e2751231b835ba
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-07 03:02:39 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,6 @@
-def current_age_for_birth_year(current_year, birth_year)
-  current_year-birth_year
+def current_age_for_birth_year(birth_year)
+  2003-birth_year
 end
 
-p current_age_for_birth_year(2003, 1984)
+p current_age_for_birth_year(1984)
 
